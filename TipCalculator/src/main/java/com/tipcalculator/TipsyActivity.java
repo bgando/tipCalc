d0897d21ce26ef871891c365b0e1706429b6404a
Bianca <gando.bia@gmail.com>
2013-09-02 17:18:42 -0700

    refactored

--- TipCalculator/src/main/java/com/tipcalculator/TipsyActivity.java
+++ TipCalculator/src/main/java/com/tipcalculator/TipsyActivity.java
@@ -1,11 +1,15 @@
 package com.tipcalculator;
 
+import java.util.Locale;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.tipcalculator.R;
 import com.tipcalculator.util.SystemUiHider;
 
 /**
@@ -22,41 +26,26 @@ public class TipsyActivity extends Activity {
         setContentView(R.layout.activity_tipsy);
     }
 
-    public void calculateFifteenPercent(View v){
+    public void calculatePercent(View v){
+    	Button clickedButton = (Button) v;
+    	String percentageString = (String) clickedButton.getTag();
+    	double percentage = Double.parseDouble(percentageString);
+
         EditText textBoxView = (EditText) findViewById(R.id.enterTotal);
         String stringTotal = textBoxView.getText().toString();
-        int total = Integer.parseInt(stringTotal);
+        double total = Double.parseDouble(stringTotal);
 
-        double tip = (total * .15);
-
-        showTip(tip);
-
-    }
-
-    public void calculateEighteenPercent(View v){
-        EditText textBoxView = (EditText) findViewById(R.id.enterTotal);
-        String stringTotal = textBoxView.getText().toString();
-        int total = Integer.parseInt(stringTotal);
-
-        double tip = (total * .18);
+        double tip = (total * percentage);
 
         showTip(tip);
+        
+        textBoxView.setText("");
 
-    }
-    
-    public void calculateTwentyPercent(View v){
-    	EditText textBoxView = (EditText) findViewById(R.id.enterTotal);
-    	String stringTotal = textBoxView.getText().toString();
-    	int total = Integer.parseInt(stringTotal);
-    	
-    	double tip = (total * .20);
-    	
-    	showTip(tip);
     }
   
     public void showTip(double num){
         TextView tipView = (TextView) findViewById(R.id.tipView);
-        String numberString = String.format("%.2f", num);
+        String numberString = String.format(Locale.getDefault(), "%.2f", num);
         tipView.setText("$" + numberString);
     }
 }
